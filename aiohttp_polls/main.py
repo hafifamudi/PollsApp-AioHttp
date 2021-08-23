c143e9840f1c7ebdc14cef9bd6d01fa22be4809c
hafifamudi <hafifcyber@gmail.com>
2021-08-23 09:30:15 +0800

    add: template jinja2 (template engine)

--- aiohttp_polls/main.py
+++ aiohttp_polls/main.py
@@ -1,9 +1,27 @@
 from aiohttp import web
 
-from settings import config
+from settings import BASE_DIR, config
 from routes import setup_routes
+from db import pg_context
 
+import aiohttp_jinja2
+import jinja2
+
+# create web instance
 app = web.Application()
+
+# create routes setup
 setup_routes(app)
+
+# db clean up context
+app.cleanup_ctx.append(pg_context)
+
+# load config .yaml
 app['config'] = config
+
+#load the templates
+aiohttp_jinja2.setup(app,
+                    loader = jinja2.FileSystemLoader(str(BASE_DIR / 'aiohttp_polls' / 'templates')))
+
+# run the app with all configuration
 web.run_app(app)
--- aiohttp_polls/views.py
+++ aiohttp_polls/views.py
@@ -1,4 +1,9 @@
 from aiohttp import web
+import db
 
 async def index(request):
-    return web.Response(text="Hello world from AioHttp")
+    async with request.app['db'].acquire() as conn:
+        cursor = await conn.execute(db.question.select())
+        records = await cursor.fetchall()
+        questions = [dict(q) for q in records]
+        return web.Response(text=str(questions))
